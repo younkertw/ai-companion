b2d4f38033885e29391197a5cfe6fc9b83a2959e
younkertw <49524985+younkertw@users.noreply.github.com>
2023-08-26 06:04:45 +0800

    add observations

--- app/(chat)/(routes)/chat/[chatId]/page.tsx
+++ app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -40,15 +40,6 @@ const ChatIdPage = async ({
           userId,
         },
       },
-      observations: {
-        orderBy: {
-          createdAt: "asc",
-        },
-        where: {
-          userId,
-        },
-        take: 2,
-      },
       _count: {
         select: {
           messages: true,
@@ -66,7 +57,7 @@ const ChatIdPage = async ({
   <div className="flex max-w-screen min-h-screen max-h-screen antialiased">
     <div className="hidden lg:block md:flex w-[45%] flex-grow items-start pt-[2.75rem] px-1 overflow-y-auto division">
       <div className="h-[60%] flex flex-col justify-center overflow-y-auto">
-           <ObservationNew observationMessages={companion.observations} /> 
+           <ObservationNew observationMessages={companion.messages} /> 
       </div>
       <div>
       </div>
